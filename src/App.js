a4d022fe87e857525933c66d82a9ef41bae2d889
Ife Olulesi <53560106+IfeOlulesi@users.noreply.github.com>
2021-09-15 16:55:42 +0100

    Testing colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return(
-    <p>This is the react application</p>
-  )
-}
+  return (
+    <div
+      style={{
+        margin: 0,
+        backgroundColor: "#261C2C",
+        height: "100vh",
+        color: "#6E85B2",
+        padding: "10px",
+        // boxSizing: "border-box",
+      }}
+    >
+      This is the react application
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Colors.js
@@ -0,0 +1,7 @@
+export const background = "#6E85B2";
+
+export const offset1 = "#5C527F";
+
+export const offset2 = "#3E2C41";
+
+export const offset3 = "#261C2C";
